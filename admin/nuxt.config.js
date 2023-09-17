8e2bedbf28c673a32dd53ac5b084d0c5cd195fda
Mook.gm99 <143313748+gmth6789@users.noreply.github.com>
2023-09-17 19:10:24 +0000

    origin

---
+++ admin/nuxt.config.js
@@ -0,0 +1,86 @@
+import colors from "vuetify/es5/util/colors";
+
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  mode: "spa",
+  //ssr: false,
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: "%s - nuxt-firebase",
+    title: "แพลตฟอร์มสร้างรายได้ออนไลน์",
+    htmlAttrs: {
+      lang: "en",
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    "@nuxtjs/vuetify",
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    [
+      "@nuxtjs/firebase",
+      {
+        config: {
+          apiKey: "AIzaSyAhJRxARUydUqR9k575ohN7MuC6lLjF0mM",
+          authDomain: "app-gm-4c8b2.firebaseapp.com",
+          databaseURL: "https://app-gm-4c8b2-default-rtdb.asia-southeast1.firebasedatabase.app",
+          projectId: "app-gm-4c8b2",
+          storageBucket: "app-gm-4c8b2.appspot.com",
+          messagingSenderId: "252378335425",
+          appId: "1:252378335425:web:55366725d68a29cc536c40",
+          measurementId: "G-PL2JFFBN7N"
+        },
+        services: {
+          auth: {
+            initialize: {
+              onAuthStateChangedMutation: "ON_AUTH_STATE_CHANGED_MUTATION",
+            },
+          },
+        },
+      },
+    ],
+  ],
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ["~/assets/variables.scss"],
+    theme: {
+      dark: false,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
+      },
+    },
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+};
---
+++ admin/plugins/vant.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import { Form, Field, CellGroup } from 'vant';
+
+const app = createApp();
+app.use(Form);
+app.use(Field);
+app.use(CellGroup);
