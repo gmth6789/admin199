5f07e3da44bb468a09d0bd1ff18e5eefb95ab72a
Mook.gm99 <143313748+gmth6789@users.noreply.github.com>
2023-09-17 19:34:38 +0000

    origin

--- admin/nuxt.config.js
+++ admin/nuxt.config.js
@@ -22,7 +22,13 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '@/assets/css/responsive.css',
+    '@/assets/css/style.css',
+    '@/assets/css/typography.css',
+    '@/assets/css/bootstrap.min.css'
+
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
